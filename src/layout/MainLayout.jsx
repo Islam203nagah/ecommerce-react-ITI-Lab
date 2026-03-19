c8e0f1e477a55a3788a5d5f1f419ce83fcf87023
Islam <a.01092664268@gmail.com>
2026-03-19 18:50:18 +0200

    fix-alis

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,9 @@
-import Footer from '@/components/ui/footer';
-import Navbar from '@/components/ui/Navbar';
-import { useThemeStore } from '@/store/useThemeStore';
+import Footer from '../components/ui/footer';
+import Navbar from '../components/ui/Navbar';
+import { useThemeStore } from '../store/useThemeStore';
 import React from 'react'
 import { Outlet } from 'react-router';
-function MainLayout() {
+function MainLayout () {
   const theme=useThemeStore((state)=>(state.theme));
   return (
     <div style={theme === "light" ? { backgroundColor: "#fff", color: "#000" } : { backgroundColor: "#333", color: "#fff" }} className='min-h-screen flex flex-col'>
